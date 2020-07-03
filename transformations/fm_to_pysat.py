3f46981499613f1a8bb494e27095311e719d83f6
José Á. Galindo <jagalindo@us.es>
2020-07-03 13:11:58 +0200

    adding support for OR and Alternative

--- transformations/fm_to_pysat.py
+++ transformations/fm_to_pysat.py
@@ -1,3 +1,5 @@
+import sys
+
 from core.models.VariabilityModel import VariabilityModel
 from core.transformations.ModelToModel import ModelToModelTransformation
 from fm_metamodel.model.FeatureModel import Feature, FeatureModel, Relation
@@ -31,12 +33,34 @@ class Fm_to_pysat(ModelToModelTransformation):
                     self.model2.variables.get(relation.parent.name)])
 
 
-        elif (relation.isSet()):
-            pass
-        elif (relation.isAlternate()):
-            pass
-        else:
-            pass
+        elif (relation.isOr()):#this is a 1 to n relatinship with multiple childs
+            
+             #add the first cnf 	child1 or child2 or ... or childN or no parent)        
+            alt_cnf=[-1*self.model2.variables.get(relation.parent.name)] #first elem of the constraint
+            for child in relation.children :
+                alt_cnf.append(self.model2.variables.get(child.name))
+            self.cnf.append(alt_cnf)
+
+            for child in relation.children:
+                self.cnf.append([-1*self.model2.variables.get(child.name),self.model2.variables.get(relation.parent.name)])
+
+        elif (relation.isAlternate()): #this is a 1 to 1 relatinship with multiple childs
+
+            #add the first cnf 	child1 or child2 or ... or childN or no parent)        
+            alt_cnf=[-1*self.model2.variables.get(relation.parent.name)] #first elem of the constraint
+            for child in relation.children :
+                alt_cnf.append(self.model2.variables.get(child.name))
+            self.cnf.append(alt_cnf)
+
+            for i in range(len(relation.children)):
+                for j in range(i+1,len(relation.children)):
+                    if i!=j:
+                        self.cnf.append([-1*self.model2.variables.get(relation.children[i].name),-1*self.model2.variables.get(relation.children[j].name)])
+                self.cnf.append([-1*self.model2.variables.get(relation.children[i].name),self.model2.variables.get(relation.parent.name)])
+                
+
+        else: #This is a m to n relationship
+            print("fatal error. N to M relationships are not yet supported in PySAT", file=sys.stderr)
         
     def transform(self):
         for feature in self.model1.get_features():
