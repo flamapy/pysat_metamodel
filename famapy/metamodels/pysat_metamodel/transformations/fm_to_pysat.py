b4fe872b7d989b55a5dc34ac2f4df01d7d65c317
GermanOctako <germanoctako@gmail.com>
2021-06-05 16:09:32 +0200

    Added the trasformation of any propositional logic to cnf clauses

--- famapy/metamodels/pysat_metamodel/transformations/fm_to_pysat.py
+++ famapy/metamodels/pysat_metamodel/transformations/fm_to_pysat.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Any
 
 from famapy.core.exceptions import ElementNotFound
 from famapy.core.models import VariabilityModel
@@ -87,21 +88,109 @@ class FmToPysat(ModelToModel):
             )
             raise NotImplementedError
 
+    def combinator(self, cnfs_left: Any, cnfs_rigth: Any, actual_op: str):
+        '''
+        Este metodo se encarga de conseguir las clausulas CNF resultantes por la combinacion de los
+        resultados entre diferentes tipos de operados logicos.
+        '''
+        result = []
+        if isinstance(cnfs_left, list) and isinstance(cnfs_rigth, list):
+            if actual_op == 'and':
+                for cnf_left in cnfs_left:
+                    result.append(cnf_left)
+                for cnf_rigth in cnfs_rigth:
+                    result.append(cnf_rigth)
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
+                for result1 in cnfs_left:
+                    for result2 in cnfs_rigth:
+                        cnf = [x for x in result1]
+                        cnf.extend(result2)
+                        result.append(cnf)
+        elif isinstance(cnfs_left, int) and isinstance(cnfs_rigth, list):
+            if actual_op == 'and':
+                result.append([cnfs_left])
+                for cnf_rigth in cnfs_rigth:
+                    result.append(cnf_rigth)
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
+                for cnf_rigth in cnfs_rigth:
+                    cnf = [cnfs_left]
+                    cnf.extend(cnf_rigth)
+                    result.append(cnf)
+        elif isinstance(cnfs_left, list) and isinstance(cnfs_rigth, int):
+            if actual_op == 'and':
+                result.append(cnfs_rigth)
+                for cnf_left in cnfs_left:
+                    result.append(cnf_left)
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
+                for cnf_left in cnfs_left:
+                    cnf = cnf_left
+                    cnf.append(cnfs_rigth)
+                    result.append(cnf)
+        else: #left int and right int
+            if actual_op == 'and':
+                result.append([cnfs_left])
+                result.append([cnfs_rigth])
+            elif actual_op in ('or', 'requires', 'implies', 'excludes'):
+                result = [[cnfs_left,cnfs_rigth]]
+        return result
+
+    def ast_iterator(self, ctc, node, number):
+        '''
+        La variable number se utiliza para seguir las leyes de morgan expuestas a continuación.
+        Reglas de las leyes de Morgan:
+            A <=> B      = (A => B) AND (B => A)
+            A  => B      = NOT(A) OR  B
+            NOT(A AND B) = NOT(A) OR  NOT(B) 
+            NOT(A OR  B) = NOT(A) AND NOT(B) 
+        '''
+        result = []
+        name = node.get_name()
+        childs = ctc.ast.get_childs(node)
+        if name == 'and':
+            cnfs_left = self.ast_iterator(ctc, childs[0], number)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number)
+            result = self.combinator(cnfs_left, cnfs_rigth, name if number > 0 else 'or')
+        elif name == 'or':
+            cnfs_left = self.ast_iterator(ctc, childs[0], number)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number)
+            result = self.combinator(cnfs_left, cnfs_rigth, name if number > 0 else 'and')
+        elif name in ('requires', 'implies'):
+            cnfs_left = self.ast_iterator(ctc, childs[0], number * -1)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number)
+            result = self.combinator(cnfs_left, cnfs_rigth, name)
+        elif name == 'excludes':
+            cnfs_left = self.ast_iterator(ctc, childs[0], number * -1)
+            cnfs_rigth = self.ast_iterator(ctc, childs[1], number * -1)
+            result = self.combinator(cnfs_left, cnfs_rigth, name)
+        elif name == 'not':
+            var = self.destination_model.variables.get(
+                ctc.ast.get_childs(node)[0].get_name()
+            )
+            if var:
+                result = -var * number
+            else:
+                cnfs = self.ast_iterator(ctc, childs[0], number * -1)
+                result = cnfs
+        else:
+            var = self.destination_model.variables.get(node.get_name())
+            result = var * number
+        return result
+
     def add_constraint(self, ctc):
-        dest = self.destination_model.variables.get(
-            ctc.ast.get_childs(ctc.ast.get_root())[1].get_name()
-        )
-        orig = self.destination_model.variables.get(
-            ctc.ast.get_childs(ctc.ast.get_root())[0].get_name()
-        )
+        node = ctc.ast.get_root()
+        if node.get_name() in ('and', 'or', 'requires', 'excludes', 'implies', 'not'):
+            cnfs = self.ast_iterator(ctc, node, 1)
+        else:
+            print('This FM contains non supported elements', file=sys.stderr)
 
-        if dest is None or orig is None:
-            raise ElementNotFound
-        if ctc.ast.get_root().get_name() == 'requires':
-            self.r_cnf.append([-1 * orig, dest])
-
-        elif ctc.ast.get_root().get_name() == 'excludes':
-            self.r_cnf.append([-1 * orig, -1 * dest])
+        if isinstance(cnfs, list):
+            for cnf in cnfs:
+                if isinstance(cnf, list):
+                    self.ctc_cnf.append(cnf)
+                else:
+                    self.ctc_cnf.append([cnf])
+        else:
+            self.ctc_cnf.append([cnfs])
 
     def transform(self):
         for feature in self.source_model.get_features():
