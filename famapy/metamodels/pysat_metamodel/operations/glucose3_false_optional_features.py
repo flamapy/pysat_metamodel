41260df63f1fd34d1b15b6a73f53d664dacb11e5
José Miguel Horcas <jhorcas@us.es>
2021-11-12 14:10:19 +0100

    Reimplemented false-optional features operation

--- famapy/metamodels/pysat_metamodel/operations/glucose3_false_optional_features.py
+++ famapy/metamodels/pysat_metamodel/operations/glucose3_false_optional_features.py
@@ -8,35 +8,32 @@ from famapy.metamodels.pysat_metamodel.models.pysat_model import PySATModel
 
 class Glucose3FalseOptionalFeatures(FalseOptionalFeatures):
 
-    def __init__(self) -> None:
-        self.false_optional_features: list[list[Any]] = []
-
-    def get_false_optional_features(self) -> list[list[Any]]:
-        return self.false_optional_features
-
-    def get_result(self) -> list[list[Any]]:
-        return self.get_false_optional_features()
+    def __init__(self, optional_features: list[str]) -> None:
+        self.result: list[Any] = []
+        self.optional_features = optional_features
 
     def execute(self, model: PySATModel) -> 'Glucose3FalseOptionalFeatures':
-        glucose_r = Glucose3()
-        glucose_r_ctc = Glucose3()
-        for clause in model.r_cnf:
-            glucose_r.add_clause(clause)
-            glucose_r_ctc.add_clause(clause)
-        for clause in model.ctc_cnf:
-            glucose_r_ctc.add_clause(clause)
-
-        if glucose_r_ctc.solve():
-            assumption = 1
-            for feat in model.features:
-                if (
-                    not glucose_r_ctc.solve(assumptions=[-feat]) and
-                    glucose_r.solve(assumptions=[-feat])
-                ):
-                    if glucose_r.solve(assumptions=[assumption, -feat]):
-                        self.false_optional_features.append(model.features.get(feat))
-                    assumption = feat
-
-        glucose_r.delete()
-        glucose_r_ctc.delete()
+        self.result = get_false_optional_features(model, self.optional_features)
         return self
+
+    def get_false_optional_features(self) -> list[list[Any]]:
+        return self.get_result()
+
+    def get_result(self) -> list[Any]:
+        return self.result
+
+
+def get_false_optional_features(model: PySATModel, optional_features: list[str]) -> list[Any]:
+    result = []
+    solver = Glucose3()
+    for clause in model.get_all_clauses():
+        solver.add_clause(clause)
+    
+    for feature in optional_features:
+        variable = model.variables.get(feature)
+        assert variable is not None
+        satisfiable = solver.solve(assumptions=[-variable])
+        if not satisfiable:
+            result.append(feature)
+    solver.delete()
+    return result
