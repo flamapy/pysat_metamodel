829e9e81af4fa14c0f652ce6babf3b1b16dfcb8c
José Miguel Horcas <jhorcas@us.es>
2021-11-12 14:09:37 +0100

    Refactor pysat transformations
    Clauses shouldn't be splitted in the PYSAT model

--- famapy/metamodels/pysat_metamodel/models/pysat_model.py
+++ famapy/metamodels/pysat_metamodel/models/pysat_model.py
@@ -12,16 +12,19 @@ class PySATModel(VariabilityModel):
         return 'pysat'
 
     def __init__(self) -> None:
-        self.r_cnf = CNF()
-        self.ctc_cnf = CNF()
-        self.variables: dict[str, Any] = {}
-        self.features: dict[str, Any] = {}
+        #self.r_cnf = CNF()  # ToDo: This should be avoid
+        #self.ctc_cnf = CNF()  # ToDo: This should be avoid
+        self._cnf = CNF()
+        self.variables: dict[str, int] = {}  # feature's name -> id
+        self.features: dict[int, str] = {}  # id -> feature's name
 
     def add_clause(self, clause: list[int]) -> None:
-        self.ctc_cnf.append(clause)
+        #self.ctc_cnf.append(clause)
+        self._cnf.append(clause)
 
     def get_all_clauses(self) -> CNF:
-        clauses = CNF()
-        clauses.extend(self.r_cnf.clauses)
-        clauses.extend(self.ctc_cnf.clauses)
-        return clauses
+        #clauses = CNF()
+        #clauses.extend(self.r_cnf.clauses)
+        #clauses.extend(self.ctc_cnf.clauses)
+        #return clauses
+        return self._cnf
