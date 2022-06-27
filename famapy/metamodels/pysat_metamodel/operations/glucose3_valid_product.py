2ff6f52537601ec08043e1aa0db1eb6472db01fa
Víctor Ramírez de la Corte <virako.9@gmail.com>
2022-06-27 09:52:14 +0200

    fix: import and linter errors

--- famapy/metamodels/pysat_metamodel/operations/glucose3_valid_product.py
+++ famapy/metamodels/pysat_metamodel/operations/glucose3_valid_product.py
@@ -1,7 +1,7 @@
 from pysat.solvers import Glucose3
 
 from famapy.core.operations import ValidProduct
-from core.famapy.metamodels.configuration_metamodel.models.configuration import Configuration
+from famapy.metamodels.configuration_metamodel.models.configuration import Configuration
 
 from famapy.metamodels.pysat_metamodel.models.pysat_model import PySATModel
 
