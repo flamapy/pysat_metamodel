8f118f8558443e63ec43454b76a4e487e175fa11
Jose A. Galndo <jagalindo@us.es>
2026-02-01 20:17:25 +0100

    refactor: standardize linting and type checking config
    - Remove mypy.ini (config now in pyproject.toml)
    - Update target-version to py39
    - Standardize setup.py to use double quotes

--- setup.py
+++ setup.py
@@ -23,15 +23,13 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/flamapy/pysat_metamodel",
-    packages=setuptools.find_namespace_packages(include=['flamapy.*']),
+    packages=setuptools.find_namespace_packages(include=["flamapy.*"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.9",
     install_requires=requirements,
-    extras_require={
-        'dev':dev_requirements
-    }
+    extras_require={"dev": dev_requirements},
 )
