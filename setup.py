a749d4ca26526942bf9d49e7972d2d60c68964fc
Jose A. Galndo <jagalindo@us.es>
2026-02-18 13:17:17 +0100

    fix: update all projects to toml.

--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
-import setuptools
+from setuptools import setup
 
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-def read_requirements(file):
-    with open(file, "r") as fh:
-        return fh.read().splitlines()
-
-# Read requirements from the requirements.txt file
-requirements = read_requirements("requirements.txt")
-
-# Read development requirements from the dev-requirements.txt file
-dev_requirements = read_requirements("requirements-dev.txt")
-
-setuptools.setup(
-    name="flamapy-sat",
-    version="2.1.0.dev1",
-    author="Flamapy",
-    author_email="flamapy@us.es",
-    description="flamapy-sat is a plugin to flamapy module",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/flamapy/pysat_metamodel",
-    packages=setuptools.find_namespace_packages(include=["flamapy.*"]),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.9",
-    install_requires=requirements,
-    extras_require={"dev": dev_requirements},
-)
+setup()
